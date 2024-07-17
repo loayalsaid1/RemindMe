653c27e09c7a9a1d356446ea723ae53be794a45b
Loay Al-Said <l.programming555@gmail.com>
2024-07-18 01:09:43 +0300

    Magnify an image and change all needed.. With a fix for scrolling mechanism

---
+++ app/static/scripts/add_reminder.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	/**
+	 * blur main section except for the froms
+	 */
+	function blur_main() {
+		$('main > *').not('.add_image_reminder').not('.add_text_reminder').css('filter', 'blur(2px)');
+	}
+	
+	/**
+	 * Showing the add text reminder form whwen chlicking the button
+	 * and the buttons to choose 
+	 */
+	$('.add_text_reminder_button').off('click').click(function(event) {
+		event.stopPropagation();
+		$('.add_image_reminder').fadeOut(100);
+		$('.add_text_reminder').fadeIn(300);
+		blur_main();
+	})
+	
+	/**
+	 * Showing the add image reminder form whwen chlicking the button
+	 * and the buttons to choose 
+	 */
+	$('.add_image_reminder_button').off('click').click(function(event) {
+		event.stopPropagation();
+		$('.add_text_reminder').fadeOut(100);
+		$('.add_image_reminder').fadeIn(300);
+		blur_main();
+	})
+		
+	/************************/
+	
+	/**
+		* Hide the form & unblur the main
+		* 
+		* 
+		* when clicking the cancel icon
+		*/
+	$('.cancel_icon').off('click').click(function(event) {
+		event.stopPropagation();
+		$(this).parent().fadeOut(300);
+		$('main > *').css('filter', 'none');
+	})
+	
+	
+})
--- app/static/scripts/user_reminders.js
+++ app/static/scripts/user_reminders.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  /**
+   * toggle the options list of a reminder
+   */
   $(".menu_icon").off("click").click(function(event) {
     event.stopPropagation(); // Prevent the event from bubbling up
     const reminderOptions = $(this).parent().find(".reminder_options");
@@ -9,5 +12,35 @@ $(document).ready(function() {
         reminderOptions.css('display', 'none');
     }
   });
+
+
+  /**
+   * magnify an image
+   */
+  $(".magnify_icon").off("click").click(function(event) {
+    event.stopPropagation();
+    const articleTag = $(this).closest('article');
+    const imgTag = articleTag.find('.reminder_image');
+    const src = imgTag.attr('src');
+    $('.fullscreen_container img').attr('src', src);
+    $('.fullscreen_container').css('display', 'flex');
+    $('.fullscreen_container').fadeIn(300);
+    blur_main();
+    
+  /**
+   * Blur the main element when clicking outside the fullscreen container
+   */
+  $('main').off('click').click(function(event) {
+    $('.fullscreen_container').fadeOut(300);
+    $('.fullscreen_container img').attr('src', '');
+    $('main > *').css('filter', 'none');
+  })
   
+  /**
+   * blur the main element
+   */
+  function blur_main() {
+	$('main > *').not('.fullscreen_container').css('filter', 'blur(5px)');
+  }
+  });
 });
