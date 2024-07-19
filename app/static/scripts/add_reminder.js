1ba422743d996ed2bb54d2cd71d64397557d84e7
Loay Al-Said <l.programming555@gmail.com>
2024-07-19 15:26:13 +0300

    position add reminder widget to the middle regardless of the scrolling position

--- app/static/scripts/add_reminder.js
+++ app/static/scripts/add_reminder.js
@@ -5,35 +5,64 @@ $(document).ready(function() {
 	function blur_main() {
 		$('main > *').not('.add_image_reminder').not('.add_text_reminder').css('filter', 'blur(2px)');
 	}
-	
+
+	/**
+	 * position add reminder windows
+	 */
+	function positionAddReminderWindows() {
+		const scrollY = $("main").scrollTop();
+		const mainHeight = $("main").height();
+		const mainWidth = $("main").width();
+  
+		$(".add_reminder_window").css({
+		  top: `${scrollY + mainHeight / 2}px`,
+		  left: `${mainWidth / 2}px`,
+		});
+	}
 	/**
 	 * Showing the add text reminder form whwen chlicking the button
-	 * and the buttons to choose 
+	 * and the buttons to choose
 	 */
 	$('.add_text_reminder_button').off('click').click(function(event) {
 		event.stopPropagation();
+		positionAddReminderWindows()
 		$('.add_image_reminder').fadeOut(100);
 		$('.add_text_reminder').fadeIn(300);
 		blur_main();
+
+		$("main")
+        .off("click")
+        .click(function (event) {
+          $(".add_reminder_window").fadeOut(300);
+          $("main > *").css("filter", "none");
+        });
 	})
-	
+
 	/**
 	 * Showing the add image reminder form whwen chlicking the button
-	 * and the buttons to choose 
+	 * and the buttons to choose
 	 */
 	$('.add_image_reminder_button').off('click').click(function(event) {
 		event.stopPropagation();
+		positionAddReminderWindows()
 		$('.add_text_reminder').fadeOut(100);
 		$('.add_image_reminder').fadeIn(300);
 		blur_main();
+
+		$("main")
+        .off("click")
+        .click(function (event) {
+          $(".add_reminder_window").fadeOut(300);
+          $("main > *").css("filter", "none");
+        });
 	})
-		
+
 	/************************/
-	
+
 	/**
 		* Hide the form & unblur the main
-		* 
-		* 
+		*
+		*
 		* when clicking the cancel icon
 		*/
 	$('.cancel_icon').off('click').click(function(event) {
@@ -41,6 +70,6 @@ $(document).ready(function() {
 		$(this).parent().fadeOut(300);
 		$('main > *').css('filter', 'none');
 	})
-	
-	
+
+
 })
--- app/static/scripts/user_reminders.js
+++ app/static/scripts/user_reminders.js
@@ -1,64 +1,67 @@
-$(document).ready(function() {
+$(document).ready(function () {
   /**
    * toggle the options list of a reminder
    */
-  $(".menu_icon").off("click").click(function(event) {
-    event.stopPropagation(); // Prevent the event from bubbling up
-    const reminderOptions = $(this).parent().find(".reminder_options");
+  $(".menu_icon")
+    .off("click")
+    .click(function (event) {
+      event.stopPropagation(); // Prevent the event from bubbling up
+      const reminderOptions = $(this).parent().find(".reminder_options");
 
-    if (reminderOptions.css('display') === 'none') {
-        reminderOptions.css('display', 'flex');
-    } else {
-        reminderOptions.css('display', 'none');
-    }
-  });
-
+      if (reminderOptions.css("display") === "none") {
+        reminderOptions.css("display", "flex");
+      } else {
+        reminderOptions.css("display", "none");
+      }
+    });
 
   /**
    * magnify an image
    */
-  $(".magnify_icon").off("click").click(function(event) {
-    event.stopPropagation();
-    const articleTag = $(this).closest('article');
-    if (articleTag.data('type') == 'image') {
-      const imgTag = articleTag.find('.reminder_image');
-      const src = imgTag.attr('src');
-      $('.fullscreen_container img').attr('src', src);
-      $('.fullscreen_container img').show();
+  $(".magnify_icon")
+    .off("click")
+    .click(function (event) {
+      event.stopPropagation();
+      const articleTag = $(this).closest("article");
+      if (articleTag.data("type") == "image") {
+        const imgTag = articleTag.find(".reminder_image");
+        const src = imgTag.attr("src");
+        $(".fullscreen_container img").attr("src", src);
+        $(".fullscreen_container img").show();
+      } else {
+        const text = articleTag.find(".shown p").text();
+        $(".fullscreen_container p").text(text);
+        $(".fullscreen_container p").show();
+      }
+      $(".fullscreen_container").css("display", "flex");
+      $(".fullscreen_container").fadeIn(300);
+      blur_main();
 
-    } else {
-      const text = articleTag.find('.shown p').text();
-      $('.fullscreen_container p').text(text);
-      $('.fullscreen_container p').show();
+      const scrollY = $("main").scrollTop();
+      const mainHeight = $("main").height();
+      const mainWidth = $("main").width();
 
+      $(".fullscreen_container").css({
+        top: `${scrollY + mainHeight / 2}px`,
+        left: `${mainWidth / 2}px`,
+      });
+      /**
+       * Blur the main element when clicking outside the fullscreen container
+       */
+      $("main")
+        .off("click")
+        .click(function (event) {
+          $(".fullscreen_container").fadeOut(300);
+          $(".fullscreen_container p").hide();
+          $(".fullscreen_container img").hide();
+          $("main > *").css("filter", "none");
+        });
+
+      /**
+       * blur the main element
+       */
+    });
+    function blur_main() {
+      $("main > *").not(".fullscreen_container").css("filter", "blur(5px)");
     }
-    $('.fullscreen_container').css('display', 'flex');
-    $('.fullscreen_container').fadeIn(300);
-    blur_main();
-    
-    const scrollY = $('main').scrollTop();
-    const mainHeight = $('main').height();
-    const mainWidth = $('main').width();
-
-    $('.fullscreen_container').css({
-      'top': `${scrollY + mainHeight / 2}px`,
-      'left': `${mainWidth / 2}px`,
-    })
-  /**
-   * Blur the main element when clicking outside the fullscreen container
-   */
-  $('main').off('click').click(function(event) {
-    $('.fullscreen_container').fadeOut(300);
-    $('.fullscreen_container p').hide();
-    $('.fullscreen_container img').hide();
-    $('main > *').css('filter', 'none');
-  })
-  
-  /**
-   * blur the main element
-   */
-  function blur_main() {
-	$('main > *').not('.fullscreen_container').css('filter', 'blur(5px)');
-  }
-  });
 });
