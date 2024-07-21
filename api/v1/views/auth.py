68c3d1e1846459f9f3f276e4f280bf1bb143fbec
Innocent Anozie <iygeal@gmail.com>
2024-07-21 16:24:23 +0100

    Added descriptive comments to auth.py in API directory

--- api/v1/views/auth.py
+++ api/v1/views/auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module implements the authentication routes"""
+"""This module implements the authentication routes
+    It also implements the upload image route using ImageKit
+"""
 
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token, jwt_required
@@ -10,15 +12,13 @@ import os
 from imagekitio import ImageKit
 
 auth = Blueprint('auth', __name__, url_prefix='/api/v1')
-upload = Blueprint('upload', __name__, url_prefix='/api/v1')
-
 
 # ImageKit Credentials
 IMAGEKIT_PRIVATE_KEY = "private_edl1a45K3hzSaAhroLRPpspVRqM="
 IMAGEKIT_PUBLIC_KEY = "public_tTc9vCi5O7L8WVAQquK6vQWNx08="
 IMAGEKIT_URL_ENDPOINT = "https://ik.imagekit.io/loayalsaid1/"
 
-
+# Create an instance of ImageKit with the credentials
 ik = ImageKit(
     private_key=IMAGEKIT_PRIVATE_KEY,
     public_key=IMAGEKIT_PUBLIC_KEY,
@@ -32,48 +32,50 @@ def login():
     email = request.json.get('email')
     password = request.json.get('password')
 
-    # import pdb; pdb.set_trace()
 
     if not email or not password:
         return jsonify({"msg": "Missing email or password"}), 400
 
-    user = storage.get_user_by_email(email)
+    user = storage.get_user_by_email(email)  # Get user from DB
 
+    # Verify password
     if not user or not user.verify_password(password):
         return jsonify({"msg": "Bad email or password"}), 401
 
+    # Create access token and return it
     access_token = create_access_token(identity=user.id)
     return jsonify(access_token=access_token), 200
 
-
-@auth.route('/refresh', methods=['POST'])  # Refresh route
-@jwt_required(refresh=True)
-def refresh():
-    """Method to refresh the access token"""
-    current_user = get_jwt_identity()  # Get current user
-    new_token = create_access_token(identity=current_user)  # Create new token
-    return jsonify(access_token=new_token), 200
+# Commented out due to dormancy of the refresh route
+# @auth.route('/refresh', methods=['POST'])  # Refresh route
+# @jwt_required(refresh=True)
+# def refresh():
+#     """Method to refresh the access token"""
+#     current_user = get_jwt_identity()  # Get current user
+#     new_token = create_access_token(identity=current_user)  # Create new token
+#     return jsonify(access_token=new_token), 200
 
 
 @auth.route('/upload', methods=['POST'])
 def upload_image():
-    """Method to upload an image"""
+    """Method to manage image upload using ImageKit"""
     if 'image' not in request.files:
         return jsonify({"error": "No image file provided"}), 400
 
-    image = request.files['image']
+    image = request.files['image']  # Get image from request
 
-    extention = image.filename.split('.')[-1]
-    temp_file_path = f'temp_image.{extention}'
-    image.save(temp_file_path)
+    extention = image.filename.split('.')[-1]  # Get image extention
+    temp_file_path = f'temp_image.{extention}'  # Create temporary file
+    image.save(temp_file_path)  # Save image to temporary file
 
+    # Upload image to ImageKit
     with open(temp_file_path, 'rb') as f:
         result = ik.upload_file(
-            file=f, file_name=f'mrnobody.{extention}')
+            file=f, file_name=f'temp_image.{extention}')
 
-    os.remove(temp_file_path)
+    os.remove(temp_file_path)  # Remove temporary file
 
-
+    # Verify if upload was successful and return image url
     if result.response_metadata.http_status_code == 200:
         image_url = result.url
         return jsonify({"url": image_url}), 200
