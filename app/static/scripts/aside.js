4ea951f12cdaa83747e0960e96e6280054b95925
Loay Al-Said <l.programming555@gmail.com>
2024-07-23 21:10:12 +0300

    Add edit profile behaviour

---
+++ app/static/scripts/aside.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.edit_profile').click(function () {
+		const message = `You can't fake your identity here...
+		Well, at least for now.. But if you insiste. here is a trick.
+		go to /finalize_profile and fill the form.
+		`;
+
+		alert(message);
+	})
+})
