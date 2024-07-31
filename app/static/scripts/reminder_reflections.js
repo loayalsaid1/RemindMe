10ec312a2e00aa9cc1cc409566cbc8d455960ae6
Loay Al-Said <l.programming555@gmail.com>
2024-07-31 22:54:07 +0300

    Change format of updated_at attibute in add reflection api response

--- app/static/scripts/reminder_reflections.js
+++ app/static/scripts/reminder_reflections.js
@@ -2,20 +2,5 @@ $(document).ready(function () {
 	$('.add_reflection').submit(function (event) {
 		event.preventDefault();
 		event.stopPropagation();
-		/**
-		 * get the data from the form.
-		 * usubmit with token to api/v1/users/user_id/reflections
-		 * 
-		 * neded data is
-		 * caption
-		 * usr_id
-		 * token
-		 * 
-		 * endpoint
-		 * remnder_id
-		 * content
-		 * 
-		 */
-
 	})
 })
