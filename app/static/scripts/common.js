814ecec6e260c29ebcd9cb1e699748db70aea2fd
Loay Al-Said <l.programming555@gmail.com>
2024-08-01 15:28:06 +0300

    Activate 'show reflection' button around the app

--- app/static/scripts/common.js
+++ app/static/scripts/common.js
@@ -18,4 +18,11 @@ function blurMain() {
 	  .css('filter', 'blur(5px)');
 }
 
+$(document).ready(function () {
+	$('main').on('click', '.show_reminder', function (event) {
+		event.stopPropagation();
+		const reminderID = $(this).closest('article').data('reminder-id');
 
+		window.location.href = `/reminders/${reminderID}`;
+	})
+})
