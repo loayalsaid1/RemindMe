dab115ffc1c3e55eceab600996c2351f18f416c9
Loay Al-Said <l.programming555@gmail.com>
2025-04-10 18:01:01 +0200

    Fix responsiveness of the new_reminder_type list
    Use a class based apporate.
    - Make a default class `hidden`
    - Manage the class with js events in aside.js

--- app/static/scripts/aside.js
+++ app/static/scripts/aside.js
@@ -1,8 +1,34 @@
-$(document).ready(function() {
-	const aside = $('aside');
+$(document).ready(function () {
+  const aside = $('aside');
+  const addReminder = aside.find('.add_reminder');
+  const chooseReminderList = aside.find('.choose_reminder_type');
 
-	$('aside .toggle_icon').on('click', function() {
-		aside.toggleClass('open');
-	})
+  const openSidebar = () => aside.addClass('open');
+  const closeSidebar = () => aside.removeClass('open');
+	const toggleSidebar = () => aside.toggleClass('open');
+  const hideChooseReminderList = () => chooseReminderList.addClass('hidden');
+  const showChooseReminderList = () => chooseReminderList.removeClass('hidden');
 
+  $('aside .toggle_icon').on('click', function () {
+    toggleSidebar();
+    hideChooseReminderList();
+  });
+
+  $('aside .choose_reminder_type button').on('click', function () {
+    closeSidebar();
+    hideChooseReminderList();
+  });
+
+  addReminder.on('mouseenter', function () {
+    if (chooseReminderList.hasClass('hidden')) {
+      showChooseReminderList();
+    }
+  });
+
+  addReminder.on('mouseleave', function () {
+    if (!chooseReminderList.hasClass('hidden')) {
+      hideChooseReminderList();
+    }
+  });
 });
+
